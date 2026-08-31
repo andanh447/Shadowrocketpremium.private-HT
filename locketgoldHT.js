d5380aaf1d4b531a650d347b80fb14f858bf9c60
andanh447 <ipxs4427@gmail.com>
2026-08-31 23:39:41 +0700

    Create locketgoldHT.js

---
+++ locketgoldHT.js
@@ -0,0 +1,4 @@
+#LocketGold
+#hostname = api.revenuecat.com
+revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/andanh447/Shadowrocketpremium.private-HT/refs/heads/main/premiumallappHT.js, requires-body=true, max-size=-1, timeout=60
+deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/duyvinh09/Module_IOS/refs/heads/main/js/deleteHeader.js, timeout=60
