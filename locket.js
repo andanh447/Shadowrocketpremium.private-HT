e56831df9dc90f783aeddb1fe6d30a8df5af290a
andanh447 <ipxs4427@gmail.com>
2026-09-01 17:15:39 +0700

    Delete locket.js

--- locket.js
+++
@@ -1,8 +0,0 @@
-[MITM]
-hostname = api.revenuecat.com
-
-[Script]
-#LocketGold
-revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/andanh447/locketgolddocquyendht/refs/heads/main/locketgoldtest.js, requires-body=true, max-size=-1, timeout=60
-deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/duyvinh09/Module_IOS/refs/heads/main/js/deleteHeader.js, timeout=60
-#************************************#
